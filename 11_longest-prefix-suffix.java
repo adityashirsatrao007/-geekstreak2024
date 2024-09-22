8f998da51bba1abe93f65e38aa2f6a6310c374a4
afiestic <afiestic@gmail.com>
2024-09-22 23:21:17 +0530

    11_longest-prefix-suffix

---
+++ 11_longest-prefix-suffix.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String s = read.readLine();
+            Solution ob = new Solution();
+            System.out.println(ob.lps(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+// User function Template for Java
+
+class Solution {
+    int lps(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int length = 0;
+        int i = 1;
+        
+        lps[0] = 0; // first character has no proper prefix suffix
+        
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(length)) {
+                length++;
+                lps[i] = length;
+                i++;
+            } else {
+                if (length != 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        
+        return lps[n - 1]; // The longest prefix which is also a suffix
+    }
+}
+
+
+
+
+
